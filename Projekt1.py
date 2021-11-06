75811a9455a6bde4a04b071041b91fb3092ab723
Martin Hromada <hromada.martin@seznam.cz>
2021-11-06 21:51:03 +0100

    Correction 1

--- Projekt1.py
+++ Projekt1.py
@@ -1,5 +1,7 @@
-TEXTS = ['''
-Situated about 10 miles west of Kemmerer, 
+from string import punctuation as pun
+
+TEXTS = [
+    '''Situated about 10 miles west of Kemmerer, 
 Fossil Butte is a ruggedly impressive 
 topographic feature that rises sharply 
 some 1000 feet above Twin Creek Valley 
@@ -7,8 +9,7 @@ to an elevation of more than 7500 feet
 above sea level. The butte is located just 
 north of US 30N and the Union Pacific Railroad, 
 which traverse the valley. ''',
-
-         '''At the base of Fossil Butte are the bright 
+    '''At the base of Fossil Butte are the bright 
 red, purple, yellow and gray beds of the Wasatch 
 Formation. Eroded portions of these horizontal 
 beds slope gradually upward from the valley floor 
@@ -16,8 +17,7 @@ and steepen abruptly. Overlying them and extending
 to the top of the butte are the much steeper 
 buff-to-white beds of the Green River Formation, 
 which are about 300 feet thick.''',
-
-         '''The monument contains 8198 acres and protects 
+    '''The monument contains 8198 acres and protects 
 a portion of the largest deposit of freshwater fish 
 fossils in the world. The richest fossil fish deposits 
 are found in multiple limestone layers, which lie some 
@@ -25,75 +25,72 @@ are found in multiple limestone layers, which lie some
 represent several varieties of perch, as well as 
 other freshwater genera and herring similar to those 
 in modern oceans. Other fish such as paddlefish, 
-garpike and stingray are also present.''']
+garpike and stingray are also present.'''
+]
 
-separator = '-' * 40
-regUsers = {'bob': '123', 'ann': 'pass123', 'mike': 'password123', 'liz': 'pass123'}
+SEPARATOR = '-' * 40
+REG_USSERS = {'bob': '123', 'ann': 'pass123', 'mike': 'password123', 'liz': 'pass123'}
 
-inptUssername = input('Ussername: ')
-inptPassword = input('Password: ')
+inpt_ussername = input('Ussername: ')
+inpt_password = input('Password: ')
 
-print(separator)
+print(SEPARATOR)
 
-if inptPassword == regUsers.get(inptUssername):
-    print(f"Welcome in app {inptUssername}.")
+if inpt_password == REG_USSERS.get(inpt_ussername):
+    print(f"Welcome in app {inpt_ussername}.")
 else:
-    print(f"Ussername or Password is not correct.\n{separator}")
+    print(f"Ussername or Password is not correct.\n{SEPARATOR}")
     exit()
 
-print(f"We have {str(len(TEXTS))} texts to be analyzed.\n{separator}")
+print(f"We have {str(len(TEXTS))} texts to be analyzed.\n{SEPARATOR}")
 
 choice = input("Enter number btw. 1 and 3 to select: ")
 
 if not choice.isnumeric():
-    print(f"Entered symbol is not number! \nClosing...\n{separator}")
+    print(f"Entered symbol is not number! \nClosing...\n{SEPARATOR}")
     exit()
-elif int(choice) not in range(1, 4):
-    print(f"Out of texts range.\nClosing...\n{separator}")
+elif int(choice) not in range(1, len(TEXTS) + 1):
+    print(f"Out of texts range.\nClosing...\n{SEPARATOR}")
     exit()
 else:
-    analyzedText = TEXTS[int(choice) - 1].split()
-    print(separator)
+    CLEAR_WORDS = [slovo.strip(pun) for slovo in TEXTS[int(choice) - 1].split()
+                   if slovo.strip(pun) != '']
 
-clearWords = []
-for slovo in analyzedText:
-    clearWord = slovo.strip(' ,.-§¨)?:_"!/(')
-    if slovo in ' ,.-§¨)?:_"!/(':
-        continue
+print(SEPARATOR)
+
+COUNT_DICT = {'word': len([word for word in CLEAR_WORDS]),
+              'numm': len([word for word in CLEAR_WORDS if word.isnumeric()]),
+              'low': len([word for word in CLEAR_WORDS if word.islower()]),
+              'tit': len([word for word in CLEAR_WORDS if word.istitle()]),
+              'upp': len([word for word in CLEAR_WORDS if word.isupper() and word.isalpha()]),
+              'nummSum': sum([int(word) for word in CLEAR_WORDS if word.isnumeric()])
+              }
+
+WORDS_LEN = {}
+
+for word in CLEAR_WORDS:
+    if len(word) not in WORDS_LEN.keys():
+        WORDS_LEN.setdefault(len(word), 1)
     else:
-        clearWords.append(clearWord)
+        WORDS_LEN[len(word)] += 1
 
-countDict = {'word': len([word for word in clearWords]),
-             'numm': len([word for word in clearWords if word.isnumeric()]),
-             'low': len([word for word in clearWords if word.islower()]),
-             'tit': len([word for word in clearWords if word.istitle()]),
-             'upp': len([word for word in clearWords if word.isupper() and word.isalpha()]),
-             'nummSum': sum([int(word) for word in clearWords if word.isnumeric()])
-             }
+print(f"""There are {COUNT_DICT.get('word')} in selected text.
+There are {COUNT_DICT.get('tit')} titlecase words.
+There are {COUNT_DICT.get('upp')} uppercase words.
+There are {COUNT_DICT.get('low')} lowercase words.
+There are {COUNT_DICT.get('numm')} numeric words.
+The summ of all the numbers is {COUNT_DICT.get('nummSum')}.""")
 
-wordsLen = {}
+print(SEPARATOR)
 
-for word in clearWords:
-    if len(word) not in wordsLen.keys():
-        wordsLen.setdefault(len(word), 1)
-    else:
-        wordsLen[len(word)] += 1
+jg1 = len(str(sorted(WORDS_LEN.keys(), reverse=True)[0])) + 1
+jg2 = sorted(WORDS_LEN.values(), reverse=True)[0] + 1
 
-print(f"""There are {countDict.get('word')} in selected text.
-There are {countDict.get('tit')} titlecase words.
-There are {countDict.get('upp')} uppercase words.
-There are {countDict.get('low')} lowercase words.
-There are {countDict.get('numm')} numeric words.
-The summ of all the numbers is {countDict.get('nummSum')}.""")
+print(f"{'LEN'.rjust(jg1)}|{'OCCURENCES'.center(jg2)}"
+      f"{'|Nr.'.ljust(jg2 - len('OCCURENCES'))}\n{SEPARATOR}")
 
-print(separator)
+for i in sorted(WORDS_LEN.keys()):
+    print(f"{str(i).rjust(jg1)}|{'*' * WORDS_LEN.get(i)}"
+          f"{'|'.rjust(jg2 + 1 - WORDS_LEN.get(i))}{str(WORDS_LEN.get(i))}")
 
-jG1 = len(str(sorted(wordsLen.keys(), reverse=True)[0])) + 1
-jG2 = sorted(wordsLen.values(), reverse=True)[0] + 1
-
-print(f"{'LEN'.rjust(jG1)}|{'OCCURENCES'.center(jG2)}{'|Nr.'.ljust(jG2 - len('OCCURENCES'))}\n{separator}")
-
-for i in sorted(wordsLen.keys()):
-    print(f"{str(i).rjust(jG1)}|{'*' * wordsLen.get(i)}{'|'.rjust(jG2 + 1 - wordsLen.get(i))}{str(wordsLen.get(i))}")
-
-print(separator)
+print(SEPARATOR)
